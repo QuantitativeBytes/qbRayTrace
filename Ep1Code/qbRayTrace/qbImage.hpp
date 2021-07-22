64206ca07be3972b4998500bacc315df771b1f3b
QuantitativeBytes <57164282+QuantitativeBytes@users.noreply.github.com>
2021-07-22 15:36:58 +0100

    Update qbImage.hpp

--- Ep1Code/qbRayTrace/qbImage.hpp
+++ Ep1Code/qbRayTrace/qbImage.hpp
@@ -1,3 +1,27 @@
+/* ***********************************************************
+
+	qbImage.hpp
+	
+	The qbImage class definition - A simple class for 2D
+	image handling.
+	
+	This file forms part of the qbRayTrace project as described
+	in the series of videos on the QuantitativeBytes YouTube
+	channel.
+	
+	This code corresponds specifically to Episode 1 of the series,
+	which may be found here:
+	https://youtu.be/JN5yUrJPThI
+	
+	The whole series may be found on the QuantitativeBytes 
+	YouTube channel at:
+	www.youtube.com/c/QuantitativeBytes
+	
+	GPLv3 LICENSE
+	Copyright (c) 2021 Michael Bennett
+	
+***********************************************************/
+
 #ifndef QBIMAGE_H
 #define QBIMAGE_H
 
